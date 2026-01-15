206af1a7f9e5ffabfc90e84eea0b91236d2c6ae1
Snix52 <emrullahs52@icloud.com>
2026-01-15 15:23:12 +0300

    Middleware ve dynamic ayari yapildi

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher(['/rehberler(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
-     // DOĞRUSU BU: auth.protect()
-     await auth.protect();
+    const client = await auth();
+    // Eğer giriş yapmamışsa, login sayfasına at
+    if (!client.userId) {
+       return client.redirectToSignIn();
+    }
   }
+  // Giriş yapmışsa veya sayfa korumalı değilse devam et
+  return NextResponse.next();
 });
 
 export const config = {
